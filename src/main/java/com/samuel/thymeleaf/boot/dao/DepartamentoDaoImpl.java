2e275ce18efdcd89e2ab37990a0ccefd9e454333
samuelkrieger <79378032+samuelkrieger@users.noreply.github.com>
2021-08-29 19:16:00 -0300

    First-commit

---
+++ src/main/java/com/samuel/thymeleaf/boot/dao/DepartamentoDaoImpl.java
@@ -0,0 +1,9 @@
+package com.samuel.thymeleaf.boot.dao;
+import org.springframework.stereotype.Repository;
+
+import com.mballem.curso.boot.domain.Departamento;
+
+@Repository
+public class DepartamentoDaoImpl extends AbstractDao<Departamento, Long> implements DepartamentoDao {
+
+}
